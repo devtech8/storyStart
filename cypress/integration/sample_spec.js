b47c5fde21183620baad371e3faec0e9715e7ed8
Mark G <devtech8@gmail.com>
2018-10-29 14:55:08 -0700

    testing integration

---
+++ cypress/integration/sample_spec.js
@@ -0,0 +1,13 @@
+//first passing test
+describe('My First Test', function() {
+  it('Does not do much!', function() {
+    expect(true).to.equal(true)
+  })
+})
+
+//first failing test
+describe('My First Test', function() {
+  it('Does not do much!', function() {
+    expect(true).to.equal(false)
+  })
+})
